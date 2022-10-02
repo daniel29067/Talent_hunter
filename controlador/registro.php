df113ee662aef67fc513ac673038db65977c04ca
JulianDavidTS <jtorressaavedra20@gmail.com>
2022-10-02 01:39:18 -0500

    02/10/2022 1:39am

--- controlador/registro.php
+++ controlador/registro.php
@@ -6,12 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Talent Hunter|Registro</title>
     <link rel="shortcut icon" href="../vista/img/talent_hunter7-removebg-preview.png">
+    <link rel="stylesheet" href="../vista/css/estiloregistro.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 </head>
 <body>
-    <img src="../vista/img//talent_hunter6.png"/>    
-    <h1>¿Que tipo de cuenta deseas crear?</h1> 
-    <a href="../controlador/registrodep.php"><input type="button" name="create_dep" value="Deportista" /></a>
-    <a href="../controlador/registroent.php"><input type="button" name="create_ent" value="Entidad" /></a>
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
 </body>
 </html>

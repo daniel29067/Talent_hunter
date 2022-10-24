01f1d5e55a0426d0721eb571aff39b8a8fa95443
JulianDavidTS <jtorressaavedra20@gmail.com>
2022-10-24 18:26:39 -0500

    Cambios 24/10/2022

--- controlador/registrodep.php
+++ controlador/registrodep.php
@@ -27,39 +27,52 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Talent Hunter|Crear Deportista</title>
+    <link rel="stylesheet" href="../vista/css/estiloredep.css">
     <link rel="shortcut icon" href="..\vista\img\talent_hunter7-removebg-preview.png">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 </head>
 <body>
-    <img src="..\vista\img\talent_hunter6.png"/>    
-<!-- formulario registro -->
+  <div class="container bg-white w-75 mt-5 mb-5 rounded shadow">
+    <div class="row align-items-stretch p-4">
 
-<form method="post" action="..\modelo\registrardep.php" enctype="multipart/form-data" >
-  <fieldset>
-    <legend  style="font-size: 18pt"><b>Registro</b></legend>
-    <!--ID-->
-    <div class="form-group">
-      <label style="font-size: 14pt"><b>Id:</b></label>
-      <input type="text" name="id" required placeholder="Ingresa tu Id" />
-    </div>
-    <!--NOMBRE-->
-    <div class="form-group">
-      <label style="font-size: 14pt"><b>Nombre:</b></label>
-      <input type="text" name="name" required placeholder="Ingresa tu nombre" pattern="[A-Za-zÑñÁÉÍÓÚáéíóú\s]+" />
-    </div>
-    <!--EMAIL-->
-    <div class="form-group">
-      <label style="font-size: 14pt; color: #000000;"><b>email:</b></label>
-      <input type="email" name="email" class="form-control" required placeholder="ingresa tu email"  />
-    </div>
-    <!--PASSWD-->
-    <div class="form-group">
-      <label style="font-size: 14pt; color: #000000;"><b>Password:</b></label>
-      <input type="password" name="pass" class="form-control" required placeholder="Ingresa contraseña" />
-    </div>
-    <!--PAIS-->
-    <div class="form-group">
-      <label style="font-size: 14pt"><b>Pais:</b></label>
-        <select required name="pais">
+      <div class="text-end mb-4">
+        <img src="../vista/img/talent_hunter6.png" width="70" alt="">
+      </div>
+
+      <div class="mb-4 text-center">
+        <h3>Registro</h3>
+      </div>
+
+      <form class="row" method="post" action="..\modelo\registrardep.php" enctype="multipart/form-data">
+        
+        <!--ID-->
+        <div class="col-lg-6 mb-2">
+          <label for="id">ID</label>
+          <input type="text" class="form-control" name="id" required placeholder="Ingresa tu Id" />
+        </div>
+
+        <!--NOMBRE-->
+        <div class="col-lg-6 mb-2">
+          <label for="nombre">Nombre</label>
+          <input type="text" class="form-control" name="name" required placeholder="Ingresa tu nombre" pattern="[A-Za-zÑñÁÉÍÓÚáéíóú\s]+" />
+        </div>
+
+        <!--EMAIL-->
+        <div class="col-lg-6 mb-2">
+          <label for="email">Email</label>
+          <input type="email" class="form-control" name="email" class="form-control" required placeholder="ingresa tu email"  />
+        </div>
+
+        <!--PASSWD-->
+        <div class="col-lg-6 mb-2">
+          <label for="">Password</label>
+          <input type="password" class="form-control" name="pass" class="form-control" required placeholder="Ingresa contraseña" />
+        </div>  
+
+        <!--PAIS-->
+        <div class="col-lg-4 mb-2">
+          <label for="pais">Pais</label>
+          <select class="form-select" required name="pais">
             <?php 
                 // use a while loop to fetch data 
                 // from the $all_categories variable 
@@ -78,39 +91,47 @@
                 endwhile; 
                 // While loop must be terminated
             ?>
-        </select>  
-    </div>
-    <!--REGION-->
-    <div class="form-group">
-      <label style="font-size: 14pt"><b>Region:</b></label>
-      <input type="text" name="region" required placeholder="Ingresa tu region" pattern="[A-Za-zÑñÁÉÍÓÚáéíóú\s]+" />
-    </div>
-    <!--DEP-->
-	<div class="form-group">
-      <label style="font-size: 14pt"><b>Deporte:</b></label>
-      <input type="text" name="deporte" required placeholder="Ingresa el Deporte" pattern="[A-Za-zÑñÁÉÍÓÚáéíóú\s]+" />
-    </div>
-    <!--POS-->
-    <div class="form-group">
-      <label style="font-size: 14pt"><b>Posicion:</b></label>
-      <input type="text" name="position" required placeholder="Ingresa la posicion" pattern="[A-Za-zzÑñÁÉÍÓÚáéíóú\s]+" />
-    </div>
-     <!--foto-->
-     <div class="form-group">
-      <label style="font-size: 14pt"><b>Foto perfil:</b></label>
-      <input required type="file" name="foto"  />
-    </div>
-    <!--descripción-->
-    <div class="form-group">
-      <label style="font-size: 14pt"><b>Descripción:</b></label>
-      <textarea name="description" rows="10" cols="50">Descripción</textarea>
-    </div>
-    <input  class="btn btn-danger" required type="submit" name="submit" value="Registrarse"/>
+          </select>  
+        </div>
 
-  </fieldset>
-</form>
-</div>
+        <!--REGION-->
+        <div class="col-lg-4 mb-2">
+          <label for="region">Region</label>
+          <input type="text" class="form-control" name="region" required placeholder="Ingresa tu region" pattern="[A-Za-zÑñÁÉÍÓÚáéíóú\s]+" />
+        </div>
 
-<!--Fin formulario registro -->
+        <!--DEP-->
+	      <div class="col-lg-4 mb-2">
+          <label for="deporte">Deporte</label>
+          <input type="text" class="form-control" name="deporte" required placeholder="Ingresa el Deporte" pattern="[A-Za-zÑñÁÉÍÓÚáéíóú\s]+" />
+        </div>
+
+        <!--POS-->
+        <div class="col-lg-4 mb-2">
+          <label for="posicion">Posicion</label>
+          <input type="text" class="form-control" name="position" required placeholder="Ingresa la posicion" pattern="[A-Za-zzÑñÁÉÍÓÚáéíóú\s]+" />
+        </div>
+
+        <!--foto-->
+        <div class="col-lg-8 mb-2">
+          <label for="fotop">Foto perfil</label>
+          <input required type="file" class="form-control" name="foto"  />
+        </div>
+
+        <!--descripción-->
+        <div class="col-lg-12 mb-4">
+          <label for="descripcion">Descripción</label>
+          <textarea name="description" class="form-control"></textarea>
+        </div>
+
+        <div class="d-grid">
+          <input class="btn btn-danger" required type="submit" name="submit" value="Registrarse"/>
+        </div>
+        
+      </form>
+
+    </div>
+  </div>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
 </body>
 </html>

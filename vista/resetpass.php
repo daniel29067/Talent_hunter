1b8e47e25a6a887fffc6771d43ba28d20d896ccf
JulianDavidTS <jtorressaavedra20@gmail.com>
2022-10-26 19:26:59 -0500

    Cambios 26/10/2022 7:26pm

--- vista/resetpass.php
+++ vista/resetpass.php
@@ -7,28 +7,47 @@
     <title>Talent Hunter|Reset password</title>
     <link rel="shortcut icon" href="../vista/img/talent_hunter7-removebg-preview.png">
     <link rel="stylesheet" href="../vista/css/estilogeneral.css">
+    <style>
+
+        @font-face{
+            font-family: 'Dancing';
+            src: url(css/fuentes/Dancing_Script/DancingScript-VariableFont_wght.ttf);
+        }
+
+        #enca{
+            align-items: center;
+            display: flex;
+            padding: 8px;
+        }
+
+    </style>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 </head>
 <body>
 
-    <div class="container bg-white w-75 mt-5 mb-5 rounded shadow">
-        <div class="row align-items-stretch p-4">
+    <div class="container bg-white w-50 mt-5 mb-5 rounded shadow">
+        <div class="row align-items-stretch">
 
-            <div class="text-end mb-4">
-                <img src="../vista/img/talent_hunter6.png" width="70" alt="">
+            <div class="header" id="enca">
+
+                    <h4>Recupera tu Cuenta</h4>
+
             </div>
 
-            <form class="row" action="../modelo/reset.php"  method="post">
+              <hr>
 
-                <div class="col mb-2">
-                    <label for="mail">Correo electrónico</label>
+            <form action="../modelo/reset.php"  method="post">
+
+                <div class="col">
+                    <label for="mail">Ingrese su correo Electronico</label>
                     <input type="email" class="form-control" name="mail">
                 </div>
-
-                <div class="d-grid">
+                
+                <div class="d-grid gap-2 d-md-flex justify-content-md-end p-2">
                     <input class="btn btn-primary" type="submit" name="reset" value="Confirmar"/>
-                </div>
-                        
+                    <input href="index.php" class="btn btn-secondary" type="submit" value="Cancelar">
+                </div>             
+
             </form>
 
         </div>
